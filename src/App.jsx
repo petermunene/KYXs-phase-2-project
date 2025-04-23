d80fd6636cd0bb1aee690c58d51c51dcc3bef9bf
petermunene <peter.munene@student.moringaschool.com>
2025-04-23 13:41:11 +0300

    displaying items

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React ,{useState,useEffect} from "react";
+import ShoeList from "./components/ShoesList";
 
-function App() {
-  const [count, setCount] = useState(0)
+ function App(){
+  const [shoeList,setShoeList]=useState([])
+  const [filteredShoes,setFilteredShoes]=useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  useEffect(()=>{
+    fetch("http://localhost:4000/shoes")
+    .then((res)=>res.json())
+    .then((data)=>{
+      setFilteredShoes(data)
+      setShoeList(data)
+    })
+  })
+
+  return(
+    <div>
+      <ShoeList shoes={filteredShoes}  />
+    </div>
   )
-}
 
-export default App
+ }
+ export default App
