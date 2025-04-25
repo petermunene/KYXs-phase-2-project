c7ea646346b0bbec9c3dd53ff41d611faad4ea60
Cornelius Chebet <cornelius.chebet@student.moringaschool.com>
2025-04-26 00:25:54 +0300

    updated Routes.jsx and added ProtectedRoute.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,60 +10,51 @@ import PasswordReset from "./components/Auth/PasswordReset";
 import Cart from "./components/Cart";
 
 function App() {
-  const [shoeList, setShoeList] = useState([]);
-  const [filteredShoes, setFilteredShoes] = useState([]);
+  const [shoes, setShoes] = useState([]);
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:4000/shoes")
       .then((res) => res.json())
-      .then((data) => {
-        setFilteredShoes(data);
-        setShoeList(data);
-      });
+      .then((data) => setShoes(data)); 
   }, []);
 
-  function handleAddToCart(order) {
-    setCart(prevCart => [...prevCart, order]);
+  function handleAddToCart(shoe) {
+    setCart(prevCart => [...prevCart, shoe]);
   }
 
   function handleRemoveFromCart(shoeToRemove) {
     setCart(prevCart => prevCart.filter(shoe => shoe.id !== shoeToRemove.id));
-    fetch(`http://localhost:4000/cart/${shoeToRemove.id}`,{
-      method:"DELETE"
+    
+    fetch(`http://localhost:4000/cart/${shoeToRemove.id}`, {
+      method: "DELETE",
     })
+    .catch(err => console.error("Failed to remove shoe:", err)); 
   }
 
   return (
-
     <AuthProvider>
       <div style={{ gap: 10, display: "flex", flexDirection: "column" }}>
         <NavBar cartCount={cart.length} />
         <Routes>
-          <Route path="/" element={
-            <ProtectedRoute>
-              <Home 
-                shoes={filteredShoes}
-                onAddShoeToCart={handleAddToCart}
-              />
-            </ProtectedRoute>
-          } />
+          {/* No ProtectedRoute around Home anymore */}
+          <Route path="/" element={<Home shoes={shoes} onAddShoeToCart={handleAddToCart} />} />
+          
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/reset-password" element={<PasswordReset />} />
+          
+          {/* ProtectedRoute around Cart */}
           <Route path="/cart" element={
             <ProtectedRoute>
-              <Cart
-                cart={cart}
-                onRemoveShoeFromCart={handleRemoveFromCart}
-              />
+              <Cart cart={cart} onRemoveShoeFromCart={handleRemoveFromCart} />
             </ProtectedRoute>
           } />
+          
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
     </AuthProvider>
-
   );
 }
 
